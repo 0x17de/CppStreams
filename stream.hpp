7f294f69bea066c0e8acfe05f9f564bba6dbeda0
Manuel Herrmann <git@icetruck.de>
2013-08-23 13:05:46 +0200

    leaks almost fixed, coredump instead

--- stream.hpp
+++ stream.hpp
@@ -1,24 +1,25 @@
 // See: https://github.com/dionyziz/stream.js/blob/master/stream.js
 
+#include <stdexcept>
 #include <functional>
+#include <memory>
 
 template <class T>
 class Stream
 {
 private:
-	typedef Stream<T>* (*fTail)();
-	T* headValue_;
-	std::function<Stream<T>*()> tailPromise_;
+	std::shared_ptr<T> headValue_;
+	std::function<std::shared_ptr<Stream<T>>()> tailPromise_;
 
 public:
-	Stream(T* headValue = nullptr, std::function<Stream<T>*()> tailPromise = nullptr)
+	Stream(std::shared_ptr<T> headValue = nullptr, std::function<std::shared_ptr<Stream<T>>()> tailPromise = nullptr)
 	{
 		headValue_ = headValue;
 		if (tailPromise != nullptr) {
 			tailPromise_ = tailPromise;
 		} else {
-			tailPromise_ = [] () -> Stream<T>* {
-				return new Stream<T>();
+			tailPromise_ = [] () -> std::shared_ptr<Stream<T>> {
+				return std::shared_ptr<Stream<T>>(new Stream<T>());
 			};
 		}
 	}
@@ -30,62 +31,63 @@ public:
 	{
 		return headValue_ != nullptr;
 	}
-	T* head()
+	std::shared_ptr<T> head()
 	{
 		return headValue_;
 	}
-	Stream<T>* tail()
+	auto tail() -> decltype(tailPromise_())
 	{
 		if (empty())
-			throw "Cannot get tail of empty stream.";
+			throw std::logic_error("Cannot get tail of empty stream.");
 		return tailPromise_(); // TODO: remember the result, set function to nullptr
 	}
 	// TODO: length, add, append, zip, map
-	Stream<T>* filter(std::function<bool(T)> f)
+	std::shared_ptr<Stream<T>> filter(std::function<bool(T)> f)
 	{
 		if (empty())
-			return new Stream<T>();
+			return std::shared_ptr<Stream<T>>(new Stream<T>());
 		auto headValue = head();
 		if (f(*headValue))
-			return new Stream<T>(headValue, [this,f] {
+			return std::shared_ptr<Stream<T>>(new Stream<T>(headValue, [this,f] {
 				return this->tail()->filter(f);
-			});
+			}));
 		return this->tail()->filter(f);	
 	}
-	Stream<T>* take(int number)
+	std::shared_ptr<Stream<T>> take(int number)
 	{
 		if (empty() || number == 0)
-			return new Stream<T>();
-		return new Stream<T>(head(), [this, number] () -> Stream<T>* {
+			return std::shared_ptr<Stream<T>>(new Stream<T>());
+		return std::shared_ptr<Stream<T>>(new Stream<T>(head(), [this, number] () -> std::shared_ptr<Stream<T>> {
 			return this->tail()->take(number - 1);
-		});
+		}));
 	}
 	template <class U>
-	Stream<U>* map(std::function<U(T)> f) {
+	std::shared_ptr<Stream<U>> map(std::function<U(T)> f) {
 		if (empty())
-			return new Stream<U>();
-		float* i = new float(f(*head()));
-		return new Stream<U>(i, [this, f] () -> Stream<U>* {
+			return std::shared_ptr<Stream<U>>(new Stream<U>());
+		return std::shared_ptr<Stream<U>>(new Stream<U>(std::shared_ptr<U>(new U(f(*head()))), [this, f] () -> std::shared_ptr<Stream<U>> {
 			return this->tail()->map(f);
-		});
+		}));
 	}
 	void walk(std::function<void(T)> f) {
-		Stream<T>* s = this;
-		while(s->avail()) {
-			f(*s->head());
-			s = s->tail();
+		if (avail()) {
+			f(*head());
+			auto s = tail();
+			while(s->avail()) {
+				f(*s->head());
+				s = s->tail();
+			}
 		}
 	}
-	static Stream<int>* makeOnes() {
-		int* i = new int(1);
-		return new Stream<int>(i, [] () -> Stream<int>* {
+	static std::shared_ptr<Stream<int>> makeOnes() {
+		T* i = new T(1);
+		return std::shared_ptr<Stream<int>>(new Stream<int>(i, [] () -> std::shared_ptr<Stream<int>> {
 			return Stream<int>::makeOnes();
-		});
+		}));
 	}
-	static Stream<int>* range(int low, int high) {
-		int* i = new int(low);
-		return new Stream<int>(i, [low, high] () -> Stream<int>* {
+	static std::shared_ptr<Stream<int>> range(int low, int high) {
+		return std::shared_ptr<Stream<int>>(new Stream<int>(std::shared_ptr<T>(new T(low)), [low, high] () -> std::shared_ptr<Stream<int>> {
 			return Stream<int>::range(low + 1, high);
-		});
+		}));
 	}
 };
