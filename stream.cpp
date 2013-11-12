b383da1719bc8cfa9aae79a0d8133a7fa173441e
Manuel Herrmann <git@icetruck.de>
2013-11-12 22:25:01 +0100

    comments for the example

--- stream.cpp
+++ stream.cpp
@@ -6,17 +6,17 @@ using namespace std;
 
 int main()
 {
-    Stream<int>::range(5)
-    .filter([] (int num)
+    Stream<int>::range(5) // build a stream from 5 to infinity
+    .filter([] (int num) // only accept even numbers
     {
         return num % 2 == 0;
     })
-    .take(5)
-    .map<float>([] (int num)
+    .take(5) // only take 5 even numbers
+    .map<float>([] (int num) // add 0.5 to each number and convert the stream to a float stream
     {
          return (float)num + 0.5f;
     })
-    .walk([] (float num)
+    .walk([] (float num) // list the results
     {
         cout << num << endl;
     });
